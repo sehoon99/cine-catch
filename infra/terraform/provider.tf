824150b2fa2ee606a38f8ccf7650504f11499800
Gyanni <lghlee36@gmail.com>
2026-01-30 16:28:48 +0900

    add terraform workflows

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
             version = "~> 5.0"
         }
     }
+
+    backend "s3" {
+        bucket = "cine-catch-terraform-state"
+        key    = "terraform.tfstate"
+        region = "ap-northeast-2"
+    }
 }
 
 provider "aws" {
